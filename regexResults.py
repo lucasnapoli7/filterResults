e27a355cdcf06b7a741d6af60559544ca7f526f4
lucasnapoli7 <72548102+lucasnapoli7@users.noreply.github.com>
2021-02-02 23:06:49 -0300

    Update regexResults.py

--- regexResults.py
+++ regexResults.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import re
-pattern = re.compile(r'^[\d]{4,4}\-\d\d\-\d\d,(.+),(.+),(\d+),(\d+).*$')
-f = open("./results.csv","r")
+pattern = re.compile(r'^[\d]{4,4}\-\d\d\-\d\d,(.+),(.+),(\d+),(\d+).*$') #Regex
+f = open("./results.csv","r") #Open file
+#Compare all lines of file
 for line in f:
-    res = re.match(pattern, line)
+    res = re.match(pattern, line) #each line is compare with the pattern
     if res:
         total = int(res.group(3)) + int(res.group(4))
         if total > 20:
